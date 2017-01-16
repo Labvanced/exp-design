68d5197351b428f030189522f5f234c6d0645182
Holger Finger <hfinger@users.noreply.github.com>
2017-01-17 00:21:47 +0100

    some UI improvements

--- koExtenders.js
+++ koExtenders.js
@@ -185,3 +185,24 @@ ko.bindingHandlers.selected = {
         if (selected) element.select();
     }
 };
+
+
+ko.bindingHandlers.singleClick= {
+    init: function(element, valueAccessor) {
+        var handler = valueAccessor(),
+            delay = 200,
+            clickTimeout = false;
+
+        $(element).click(function() {
+            if(clickTimeout !== false) {
+                clearTimeout(clickTimeout);
+                clickTimeout = false;
+            } else {
+                clickTimeout = setTimeout(function() {
+                    clickTimeout = false;
+                    handler();
+                }, delay);
+            }
+        });
+    }
+};
