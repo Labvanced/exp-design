73554487f00b3b02dd8e0fda674e6a3ecc21d29b
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2017-02-24 08:08:46 +0100

    mostly user profile stuff

--- contentElements/experimentPreview.js
+++ contentElements/experimentPreview.js
@@ -8,12 +8,36 @@ var ExperimentPreview= function(experiment) {
     this.exp_name = ko.observable(experiment.publishing_data.exp_name());
     this.jdenticonHash = ko.observable(experiment.publishing_data.jdenticonHash());
     this.description = ko.observable(experiment.publishing_data.description());
-   
 };
 
 
 
-ExperimentPreview.prototype.init = function() {
+ExperimentPreview.prototype.init = function(divWidth,divHeight) {
+
+
+
+    $('#experimentPreview').css(
+        "width",  divWidth
+    );
+    $('#experimentPreview').css(
+        "height", divHeight
+    );
+
+    $('#identicon').css(
+        "height", divHeight*0.75
+    );
+    $('#identicon').css(
+        "width", divHeight*0.75
+    );
+    $('#ownImage').css(
+        "height", divHeight*0.75
+    );
+    $('#ownImage').css(
+        "width", divHeight*0.75
+    );
+    $('#heading').css(
+        "font-size", Math.round(35*divWidth/1000)
+    );
     jdenticon();
     jdenticon.update("#identicon", this.jdenticonHash());
 };
