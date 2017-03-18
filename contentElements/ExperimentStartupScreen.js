ef814adaccc147d78e11fb3b4b9fb85a401716c1
Holger Finger <hfinger@users.noreply.github.com>
2017-03-19 00:52:56 +0100

    fixed experiment startup screen with uglifyJs

--- contentElements/ExperimentStartupScreen.js
+++ contentElements/ExperimentStartupScreen.js
@@ -1,27 +1,23 @@
-/**
- * Created by cgoeke on 23.02.2017.
- */
-
-
-var ExperimentPreview= function(experiment) {
+var ExperimentStartupScreen = function(experiment) {
     this.imageType = ko.observable(experiment.publishing_data.imageType());
     this.exp_name = ko.observable(experiment.publishing_data.exp_name());
     this.jdenticonHash = ko.observable(experiment.publishing_data.jdenticonHash());
     this.description = ko.observable(experiment.publishing_data.description());
 };
 
+ExperimentStartupScreen.prototype.dummyFunction = function() {
+    // do nothing
+};
 
-
-ExperimentPreview.prototype.init = function(divWidth,divHeight) {
-
-
-
+ExperimentStartupScreen.prototype.init = function(divWidth, divHeight) {
     $('#experimentPreview').css(
         "width",  divWidth
     );
     $('#experimentPreview').css(
         "height", divHeight
     );
+
+    console.log('initialize ExperimentStartupScreen...');
 
     $('#identicon').css(
         "height", divHeight*0.75
