d00e0cac42c8f47a2ab5011a723ba93bd77e5e28
Holger Finger <hfinger@users.noreply.github.com>
2017-04-11 15:01:36 +0200

    removed deprecated playerCallbacks.js

--- canvasViews/frameView.js
+++ canvasViews/frameView.js
@@ -218,12 +218,8 @@ FrameView.prototype.addElem = function(elementData,index) {
     var elemView = new FrameElementView(elementData, this);
     // var elemView = new CanvasFrameElementView(elementData,this); // deprecated easeljs canvas elements
 
-    var callbacks = null;
     if (this.type == "editorView") {
         var callbacks = new EditorCallbacks(elemView, this);  // is this used?
-    }
-    else if (this.type == "playerView") {
-        var callbacks = new PlayerCallbacks(elemView, this);    // is this used?
     }
     this.viewElements.splice(index, 0, elemView);
 
