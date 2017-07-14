7491543c2c30a1ec2a0948b32c3514ac527af53d
Holger Finger <hfinger@users.noreply.github.com>
2017-07-14 18:13:11 +0200

    improved button element with own trigger.

--- contentElements/ButtonElement.js
+++ contentElements/ButtonElement.js
@@ -3,29 +3,35 @@ var ButtonElement = function(expData) {
     this.expData = expData;
     this.parent = null;
 
-    //serialized
+    // serialized:
     this.type= "ButtonElement";
     this.id = ko.observable(guid());
-    this.buttonText = ko.observable("Button");
-    this.returnButton = ko.observable(true);
-    this.selected = ko.observable(false);
+
+    this.buttonEntries = ko.observableArray([]);
+    this.addButton();
 
     // modifier:
     this.modifier = ko.observable(new Modifier(this.expData, this));
 
-
-    ///// not serialized
+    // not serialized
     this.selected = ko.observable(false);
-    /////
+
 };
 
 ButtonElement.prototype.label = "Button";
 ButtonElement.prototype.iconPath = "/resources/icons/tools/tool_button.svg";
-ButtonElement.prototype.modifiableProp = ["buttonText"];
-ButtonElement.prototype.dataType =      [ "string"];
+ButtonElement.prototype.modifiableProp = [];
+ButtonElement.prototype.dataType =      [];
 
-ButtonElement.prototype.initWidth = 80;
-ButtonElement.prototype.initHeight = 38;
+ButtonElement.prototype.initWidth = 120;
+ButtonElement.prototype.initHeight = 40;
+
+
+ButtonElement.prototype.addButton = function() {
+    var button = new ButtonEntry(this);
+    button.buttonText('<div style="text-align: center;">Button</div>');
+    this.buttonEntries.push(button);
+};
 
 /**
  * This function is used recursively to retrieve an array with all modifiers.
@@ -48,10 +54,14 @@ ButtonElement.prototype.selectTrialType = function(selectionSpec) {
 };
 
 ButtonElement.prototype.toJS = function() {
+    var buttonEntries = [];
+    for (var i=0; i<this.buttonEntries().length; i++) {
+        buttonEntries.push(this.buttonEntries()[i].toJS());
+    }
     return {
         type: this.type,
         id: this.id(),
-        buttonText: this.buttonText(),
+        buttonEntries: buttonEntries,
         modifier: this.modifier().toJS()
     };
 };
@@ -59,10 +69,69 @@ ButtonElement.prototype.toJS = function() {
 ButtonElement.prototype.fromJS = function(data) {
     this.type=data.type;
     this.id(data.id);
-    this.buttonText(data.buttonText);
+
+    var buttonEntries = [];
+    if (data.hasOwnProperty('buttonText')) {
+        // converting from old format:
+        var entry = new ButtonEntry(this);
+        entry.buttonText(data.buttonText);
+        buttonEntries.push(entry);
+    }
+    else {
+        for (var i=0; i<data.buttonEntries.length; i++) {
+            var buttonEntry = new ButtonEntry(this);
+            buttonEntry.fromJS(data.buttonEntries[i]);
+            buttonEntries.push(buttonEntry);
+        }
+    }
+    this.buttonEntries(buttonEntries);
     this.modifier(new Modifier(this.expData, this));
     this.modifier().fromJS(data.modifier);
 };
+
+
+////////////////////////////////////
+
+
+
+
+var ButtonEntry= function(parent, initText) {
+    this.parent = parent;
+    this.buttonText = ko.observable( initText );
+};
+
+ButtonEntry.prototype.modifiableProp = ["buttonText"];
+ButtonEntry.prototype.dataType =[ "string"];
+
+ButtonEntry.prototype.selectTrialType = function(selectionSpec) {
+    this.modifier().selectTrialType(selectionSpec);
+};
+
+/**
+ * This function is used recursively to retrieve an array with all modifiers.
+ * @param {Array} modifiersArr - this is an array that holds all modifiers.
+ */
+ButtonEntry.prototype.getAllModifiers = function(modifiersArr) {
+};
+
+ButtonEntry.prototype.setPointers = function(entitiesArr) {
+};
+
+ButtonEntry.prototype.reAddEntities = function(entitiesArr) {
+};
+
+ButtonEntry.prototype.fromJS = function(data) {
+    this.buttonText(data.buttonText);
+    return this;
+};
+
+ButtonEntry.prototype.toJS = function() {
+    return {
+        buttonText:  this.buttonText()
+    };
+};
+
+///////////////////////////////
 
 function createButtonElementComponents() {
     ko.components.register('button-editview', {
@@ -71,6 +140,10 @@ function createButtonElementComponents() {
 
                 var viewModel = function (dataModel) {
                     this.dataModel = dataModel;
+                };
+
+                viewModel.prototype.addButton = function() {
+                    this.dataModel.addButton();
                 };
 
                 return new viewModel(dataModel);
