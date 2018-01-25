603694a5e75a157292f38169a0ea3532eb62bbe2
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2018-01-25 08:44:29 +0100

    ctrl+q optiona via checkbox in study settings

--- studySettings.js
+++ studySettings.js
@@ -4,6 +4,7 @@ var StudySettings = function (expData) {
     this.studySettings = this.expData.studySettings;
     this.timeZoneOffset = ko.observable(0);
     this.bgColor = ko.observable('#ffffff')
+    this.allowSTRGQ = ko.observable(true);
 };
 StudySettings.prototype.timezones = ["-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0","+1","+2","+3","+4","+5","+6","+7","+8","+9","+10","+11","+12"];
 
@@ -24,6 +25,10 @@ StudySettings.prototype.fromJS = function(data) {
     if (data.hasOwnProperty("bgColor")){
         this.bgColor(data.bgColor);
     }
+    if (data.hasOwnProperty("allowSTRGQ")){
+        this.allowSTRGQ(data.allowSTRGQ);
+    }
+
 
 
 };
@@ -32,7 +37,8 @@ StudySettings.prototype.fromJS = function(data) {
 StudySettings.prototype.toJS = function() {
     var data = {
         timeZoneOffset: this.timeZoneOffset(),
-        bgColor:this.bgColor()
+        bgColor:this.bgColor(),
+        allowSTRGQ:this.allowSTRGQ()
     };
     return data;
 };
