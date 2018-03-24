544386b0052268089e26956f6ab84002f03da069
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2018-03-24 07:41:39 +0100

    participant consent optional, with warning

--- studySettings.js
+++ studySettings.js
@@ -36,6 +36,8 @@ var StudySettings = function (expData) {
     // change these values to refactor the min/max number of participants
     this.minNumPartOfJointExp = ko.observable(2);
     this.maxNumPartOfJointExp = ko.observable(10);
+
+    this.participantConsent = ko.observable(true);
 };
 StudySettings.prototype.timezones = ["-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0","+1","+2","+3","+4","+5","+6","+7","+8","+9","+10","+11","+12"];
 
@@ -136,6 +138,11 @@ StudySettings.prototype.fromJS = function(data) {
     if (data.hasOwnProperty("minHeight")){
         this.minHeight(data.minHeight);
     }
+    if (data.hasOwnProperty("participantConsent")){
+        this.participantConsent(data.participantConsent);
+    }
+
+
 };
 
 
@@ -162,7 +169,8 @@ StudySettings.prototype.toJS = function() {
         allowEdge:this.allowEdge(),
         minRes:this.minRes(),
         minWidth:this.minWidth(),
-        minHeight:this.minHeight()
+        minHeight:this.minHeight(),
+        participantConsent:this.participantConsent()
     };
     return data;
 };
