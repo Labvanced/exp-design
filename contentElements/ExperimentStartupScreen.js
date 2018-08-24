847a59aa8ccca69fae367e179309896f69f4fcc1
Holger Finger <hfinger@users.noreply.github.com>
2018-08-24 15:38:20 +0200

    fix  text algin on mobile when starting exp.

--- contentElements/ExperimentStartupScreen.js
+++ contentElements/ExperimentStartupScreen.js
@@ -626,16 +626,9 @@ ExperimentStartupScreen.prototype.startExp = function() {
 
     player.startFullscreen();
 
-    $('#sectionPreload').css({
-        position:"absolute",
-        margin: "0 auto",
-        height:"100%",
-        width:"100%",
-        left:"40%",
-        top:"50%"
-    });
-
-    $('#sectionPreload').html("<div style='font-size: xx-large;'>"+this.expData.staticStrings().startingExp+"</div>");
+    $('#sectionPreload').html("<div style='position: fixed; width: 100%; height: 100%;'>" +
+        "<div style='margin: 0; position: absolute; top: 50%; left: 50%;margin-right: -50%; transform: translate(-50%, -50%); font-size: xx-large;'>"+this.expData.staticStrings().startingExp+"</div>" +
+        "</div>");
     $("#startExpSection").hide();
 
     // wait for five seconds:
