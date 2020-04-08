b3965f1298bdfc41f66b753bcac5cb3d94e16c33
Skylaxxx <caspar.goeke@gmail.com>
2020-04-08 19:10:23 +0700

    use iframe icon

--- contentElements/IFrameElement.js
+++ contentElements/IFrameElement.js
@@ -1,24 +1,24 @@
 
-var IFrameElement = function(expData) {
+var IFrameElement = function (expData) {
     this.expData = expData;
     this.parent = null;
     this.expData = expData;
 
-    this.type= "IFrameElement";
-    this.iFrameUrl =  ko.observable("");
+    this.type = "IFrameElement";
+    this.iFrameUrl = ko.observable("");
     this.variable = ko.observable(null);
     this.modifier = ko.observable(new Modifier(this.expData, this));
 
 };
 
 IFrameElement.prototype.label = "I-Frame";
-IFrameElement.prototype.iconPath = "/resources/icons/tools/tool_text.svg";
+IFrameElement.prototype.iconPath = "/resources/icons/tools/tool_iframe.svg";
 IFrameElement.prototype.numVarNamesRequired = 0;
-IFrameElement.prototype.dataType =      ["string"];
-IFrameElement.prototype.modifiableProp = ["iFrameUrl" ];
+IFrameElement.prototype.dataType = ["string"];
+IFrameElement.prototype.modifiableProp = ["iFrameUrl"];
 
 
-IFrameElement.prototype.init = function() {
+IFrameElement.prototype.init = function () {
 
 };
 
@@ -26,20 +26,20 @@ IFrameElement.prototype.init = function() {
  * This function is used recursively to retrieve an array with all modifiers.
  * @param {Array} modifiersArr - this is an array that holds all modifiers.
  */
-IFrameElement.prototype.getAllModifiers = function(modifiersArr) {
+IFrameElement.prototype.getAllModifiers = function (modifiersArr) {
     modifiersArr.push(this.modifier());
 };
 
-IFrameElement.prototype.setPointers = function(entitiesArr) {
+IFrameElement.prototype.setPointers = function (entitiesArr) {
     this.modifier().setPointers(entitiesArr);
 };
 
 
-IFrameElement.prototype.reAddEntities = function(entitiesArr) {
+IFrameElement.prototype.reAddEntities = function (entitiesArr) {
     this.modifier().reAddEntities(entitiesArr);
 };
 
-IFrameElement.prototype.selectTrialType = function(selectionSpec) {
+IFrameElement.prototype.selectTrialType = function (selectionSpec) {
     this.modifier().selectTrialType(selectionSpec);
 };
 
@@ -48,7 +48,7 @@ IFrameElement.prototype.dispose = function () {
 };
 
 
-IFrameElement.prototype.toJS = function() {
+IFrameElement.prototype.toJS = function () {
     return {
         type: this.type,
         iFrameUrl: this.iFrameUrl(),
@@ -56,8 +56,8 @@ IFrameElement.prototype.toJS = function() {
     };
 };
 
-IFrameElement.prototype.fromJS = function(data) {
-    this.type=data.type;
+IFrameElement.prototype.fromJS = function (data) {
+    this.type = data.type;
     this.modifier(new Modifier(this.expData, this));
     this.modifier().fromJS(data.modifier);
     this.iFrameUrl(data.iFrameUrl);
@@ -68,42 +68,42 @@ function createIFrameElementComponents() {
     ko.components.register('iframe-editview', {
         viewModel: {
             createViewModel: function (dataModel, componentInfo) {
-                var viewModel = function(dataModel){
+                var viewModel = function (dataModel) {
                     this.dataModel = dataModel;
                     this.iFrameUrl = dataModel.iFrameUrl;
                 };
-               
+
                 return new viewModel(dataModel);
             }
 
         },
-        template: {element: 'iframe-editview-template'}
+        template: { element: 'iframe-editview-template' }
     });
 
-    ko.components.register('iframe-preview',{
+    ko.components.register('iframe-preview', {
         viewModel: {
-            createViewModel: function(dataModel, componentInfo){
-                var viewModel = function(dataModel){
-                    this.dataModel = dataModel;
-                    this.iFrameUrl = dataModel.iFrameUrl;
-                };                
-                return new viewModel(dataModel);
-            }
-        },
-        template: {element: 'iframe-preview-template'}
-    });
-
-
-    ko.components.register('iframe-playerview',{
-        viewModel: {
-            createViewModel: function(dataModel, componentInfo){
-                var viewModel = function(dataModel){
+            createViewModel: function (dataModel, componentInfo) {
+                var viewModel = function (dataModel) {
                     this.dataModel = dataModel;
                     this.iFrameUrl = dataModel.iFrameUrl;
                 };
                 return new viewModel(dataModel);
-            }          
+            }
         },
-        template: {element: 'iframe-playerview-template'}
+        template: { element: 'iframe-preview-template' }
+    });
+
+
+    ko.components.register('iframe-playerview', {
+        viewModel: {
+            createViewModel: function (dataModel, componentInfo) {
+                var viewModel = function (dataModel) {
+                    this.dataModel = dataModel;
+                    this.iFrameUrl = dataModel.iFrameUrl;
+                };
+                return new viewModel(dataModel);
+            }
+        },
+        template: { element: 'iframe-playerview-template' }
     });
 }
