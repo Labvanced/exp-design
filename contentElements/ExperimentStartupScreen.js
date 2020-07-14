116fe73b36c493160ae62e9328e92398bb52b259
caspar-goeke <caspar.goeke@gmail.com>
2020-07-14 23:46:35 +0700

    check if permissions are supported

--- contentElements/ExperimentStartupScreen.js
+++ contentElements/ExperimentStartupScreen.js
@@ -237,38 +237,40 @@ var ExperimentStartupScreen = function (experiment) {
 
         }
     }
+    if (navigator.permissions) {
+        navigator.permissions.query({ name: 'microphone' }).then(function (res) {
+            res.onchange = function (e) {
+                // detecting if the event is a change
+                if (e.type === 'change') {
+                    // checking what the new permissionStatus state is
+                    var newState = e.target.state;
+                    if (newState === 'granted') {
+                        if (window.innerHeight !== screen.height) {
+                            player.startFullscreen();
+                        }
 
-    navigator.permissions.query({ name: 'microphone' }).then(function (res) {
-        res.onchange = function (e) {
-            // detecting if the event is a change
-            if (e.type === 'change') {
-                // checking what the new permissionStatus state is
-                var newState = e.target.state;
-                if (newState === 'granted') {
-                    if (window.innerHeight !== screen.height) {
-                        player.startFullscreen();
                     }
-
                 }
-            }
-        };
-    });
+            };
+        });
 
-    navigator.permissions.query({ name: 'camera' }).then(function (res) {
-        res.onchange = function (e) {
-            // detecting if the event is a change
-            if (e.type === 'change') {
-                // checking what the new permissionStatus state is
-                var newState = e.target.state;
-                if (newState === 'granted') {
-                    if (window.innerHeight !== screen.height) {
-                        player.startFullscreen();
+        navigator.permissions.query({ name: 'camera' }).then(function (res) {
+            res.onchange = function (e) {
+                // detecting if the event is a change
+                if (e.type === 'change') {
+                    // checking what the new permissionStatus state is
+                    var newState = e.target.state;
+                    if (newState === 'granted') {
+                        if (window.innerHeight !== screen.height) {
+                            player.startFullscreen();
+                        }
+
                     }
-
                 }
-            }
-        };
-    });
+            };
+        });
+    }
+
 
 };
 
